f07c4761ef70fc597459878a9420408b568ee1c3
Stefan Reichel <stefan.reichel@covus.com>
2013-07-29 17:33:25 +0200

    Removed NuGet packages folder from .gitignore and readded Machine.Specifications.0.5.14.

--- NiceTry/Combinators.cs
+++ NiceTry/Combinators.cs
@@ -1,47 +1,56 @@
 using System;
 
-namespace NiceTry {
-    public static class Combinators {
+namespace NiceTry
+{
+    public static class Combinators
+    {
         public static ITry OrElse(this ITry result,
-                                  Action orElse) {
+                                  Action orElse)
+        {
             return result.IsFailure
                        ? Try.To(orElse)
                        : result;
         }
 
         public static ITry<TValue> OrElse<TValue>(this ITry<TValue> result,
-                                                  Func<TValue> orElse) {
+                                                  Func<TValue> orElse)
+        {
             return result.IsFailure
                        ? Try.To(orElse)
                        : result;
         }
 
         public static ITry<TValue> OrElse<TValue>(this ITry<TValue> result,
-                                                  TValue orElse) {
+                                                  TValue orElse)
+        {
             return result.IsFailure
                        ? new Success<TValue>(orElse)
                        : result;
         }
 
-        public static ITry<TNewValue> Map<TValue, TNewValue>(this ITry<TValue> t, Func<TValue, TNewValue> func) {
+        public static ITry<TNewValue> Map<TValue, TNewValue>(this ITry<TValue> t, Func<TValue, TNewValue> func)
+        {
             return t.IsFailure
-                       ? new Failure<TNewValue>(t.Error)
+                       ? t as ITry<TNewValue>
                        : Try.To(() => func(t.Value));
         }
 
-        public static ITry<TNewValue> FlatMap<TValue, TNewValue>(this ITry<TValue> t, Func<TValue, ITry<TNewValue>> func) {
+        public static ITry<TNewValue> FlatMap<TValue, TNewValue>(this ITry<TValue> t, Func<TValue, ITry<TNewValue>> func)
+        {
             return t.IsFailure
                        ? new Failure<TNewValue>(t.Error)
                        : func(t.Value);
         }
 
-        public static ITry Recover(this ITry t, Action<Exception> recover) {
+        public static ITry Recover(this ITry t, Action<Exception> recover)
+        {
             return t.IsFailure
                        ? Try.To(() => recover(t.Error))
                        : t;
         }
 
-        public static ITry<TValue> Recover<TValue>(this ITry<TValue> t, Func<Exception, TValue> func) {
+        public static ITry<TValue> Recover<TValue>(this ITry<TValue> t, Func<Exception, TValue> func)
+        {
             return t.IsFailure
                        ? Try.To(() => func(t.Error))
                        : t;
@@ -49,7 +58,8 @@ namespace NiceTry {
 
         public static ITry Transform(this ITry result,
                                      Func<ITry> whenSuccess,
-                                     Func<Exception, ITry> whenFailure) {
+                                     Func<Exception, ITry> whenFailure)
+        {
             return result.IsSuccess
                        ? whenSuccess()
                        : whenFailure(result.Error);
@@ -57,16 +67,20 @@ namespace NiceTry {
 
         public static ITry<TNewValue> Transform<TValue, TNewValue>(this ITry<TValue> result,
                                                                    Func<TValue, ITry<TNewValue>> whenSuccess,
-                                                                   Func<Exception, ITry<TNewValue>> whenFailure) {
+                                                                   Func<Exception, ITry<TNewValue>> whenFailure)
+        {
             return result.IsSuccess
                        ? whenSuccess(result.Value)
                        : whenFailure(result.Error);
         }
 
         public static ITry<TValue> Filter<TValue>(this ITry<TValue> result,
-                                                  Func<TValue, bool> predicate) {
+                                                  Func<TValue, bool> predicate)
+        {
             if (result.IsFailure)
+            {
                 return result;
+            }
 
             return result.FlatMap(
                 v => predicate(v)
