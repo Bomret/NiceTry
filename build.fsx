42f3123c2784cbf4d875a45d52cfe0cf75f785dc
Stefan Reichel <dev@bomret.com>
2014-01-09 00:03:03 +0100

    Added creation of AssemblyInfo to build script.

--- build.fsx
+++ build.fsx
@@ -1,15 +1,28 @@
 #r @"FAKE\tools\FakeLib.dll"
-open Fake 
+open Fake
+open Fake.AssemblyInfoFile
 
 RestorePackages()
 
 let buildDir = "./build"
 let testDir = "./test"
 let testAssemblies = !! (testDir + "/*.Tests.dll")
+let version = 
+    match buildServer with
+        | TeamCity -> buildVersion
+        | _ -> "2.0.0"
 
 Target "Clean" (fun _ -> CleanDirs [buildDir; testDir])
 
 Target "BuildLib" (fun _ -> 
+    CreateCSharpAssemblyInfo "./NiceTry/Properties/AssemblyInfo.cs"
+        [Attribute.Title "NiceTry"
+         Attribute.Description "A functional wrapper type for the classic try/catch statement"
+         Attribute.Guid "d9712c70-9a11-43b9-b9b4-10b4036ea8f2"
+         Attribute.Product "NiceTry"
+         Attribute.Version version
+         Attribute.FileVersion version]
+
     !! "NiceTry/**/*.csproj"
     |> MSBuildRelease buildDir "Build"
     |> Log "Build output: "
