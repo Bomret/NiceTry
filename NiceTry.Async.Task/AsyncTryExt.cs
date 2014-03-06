d4bacedfd64f70d90d188bd703bf1a80e70d44a8
Stefan Reichel <dev@bomret.com>
2014-03-06 01:22:00 +0100

    Killed the old stuff

--- NiceTry.Async.Task/AsyncTryExt.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace NiceTry.Async {
-    public static class AsyncTryExt {
-        public static TaskAwaiter<ITry<T>> GetAwaiter<T>(this AsyncTry<T> @try) {
-            return @try.Worker.GetAwaiter();
-        }
-    }
-}
--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-".nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "." "-ExcludeVersion"
-"FAKE\tools\Fake.exe" build.fsx
-pause
