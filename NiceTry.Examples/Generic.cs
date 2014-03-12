9c3f7cea6d53cf1648ca806ebb032da0ca508ef1
Stefan Reichel <dev@bomret.com>
2014-03-12 21:58:03 +0100

    Replaced the broken content with the stuff from the version-2.1 branch

---
+++ NiceTry.Examples/Generic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace NiceTry.Examples {
+    class Generic {
+        public void ReadUrlFromConsoleDownloadContentAndPrint() {
+            Try.To(() => Console.ReadLine())
+               .Map(input => new Uri(input))
+               .OrElse(new Uri("http://google.com"))
+               .Using(() => new WebClient(), (wc, url) => wc.DownloadString(url))
+               .Filter(content => string.IsNullOrWhiteSpace(content) == false)
+               .Match(content => Console.WriteLine("Success: {0}", content),
+                      error => Console.WriteLine("Failure: {0}", error.Message));
+        }
+    }
+}
