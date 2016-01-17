6ea72f3fcf33a29eb32893a8cc4b045ee0b4b2e0
Stefan Reichel <dev@bomret.com>
2016-01-17 19:49:07 +0100

    Added tests for SelectExt

---
+++ tests/NiceTry.Tests/Combinators/SelectExtTests.fs
@@ -0,0 +1,21 @@
+﻿module NiceTry.Tests.Combinators
+
+open System
+open FsCheck.NUnit
+open NiceTry
+open NiceTry.Combinators
+
+module SelectExtTests =
+    [<Property>]
+    let ``Trying to transform values that do not throw exceptions should always result in success containing the transformed value`` () =
+        Try.Success 4 = SelectExt.Select(Try.Success 3, fun i -> i + 1)
+
+    [<Property>]
+    let ``Trying to transform values that throw exceptions should always result in failure`` () =
+        let throw = fun () -> failwith "Expected err"
+        Try.Failure <| Exception "Expected err" = SelectExt.Select(Try.Success 3, fun _ -> throw ())
+
+    [<Property>]
+    let ``Trying to transform failure should always result in failure`` () =
+        let err = Exception "Expected err"
+        Try.Failure<int> err = SelectExt.Select(Try.Failure<int> err, fun i -> i + 1)
--- tests/NiceTry.Tests/Try.Creation.Tests.fs
+++ tests/NiceTry.Tests/Try.Creation.Tests.fs
@@ -3,21 +3,16 @@
 open System
 open FsCheck.NUnit
 open NiceTry
+open TheVoid
 
 module TryCreationTests =
     [<Property>]
     let ``Trying to execute actions that do not throw exceptions should always result in success`` (action : unit -> unit) =
-        Try.To action  = Try.Success ()
+        Try.To action  = Try.Success Unit.Default
 
     [<Property>]
     let ``Trying to execute functions that do not throw exceptions should always result in success`` (func : unit -> string) =
         Try.To func = Try.Success (func ())
-
-    [<Property>]
-    let ``Trying to execute functions that do not throw exceptions and return instances of ITry should always result in these instances`` () =
-        let func = fun () -> Try.Success()
-        
-        Try.To func = func ()
 
     [<Property>]
     let ``Trying to execute functions that do not throw exceptions and return instances of ITry<T> should always result in these instances`` () =
@@ -28,9 +23,9 @@ module TryCreationTests =
     [<Property>]
     let ``Trying to execute actions that throw exceptions should always result in failure`` () =
         let err = Exception "Expected err"
-        let throw = fun () -> raise err
+        let throw = fun () -> raise err; ()
 
-        Try.To throw = Try.Failure err
+        Try.To throw = Try.Failure<Unit> err
 
     [<Property>]
     let ``Trying to execute functions that throw exceptions should always result in failure`` () =
