c2e03ea832070a8faf3883d15e7dbf4cd3504d6c
Stefan Reichel <dev@bomret.com>
2016-02-14 19:22:54 +0100

    Added tests for Finally.

---
+++ tests/NiceTry.Tests/Combinators/FinallyExt.Tests.fs
@@ -0,0 +1,40 @@
+﻿namespace NiceTry.Tests.Combinators
+
+open NUnit.Framework
+open NiceTry
+open NiceTry.Combinators
+open System
+open TheVoid
+
+module FinallyExtTests = 
+    [<Test>]
+    let ``Executing a finally side effect on a success should indeed execute it``() = 
+        let original = Try.Success 3
+        let mutable wasSideEffectExecuted = false
+        let result = original.Finally(fun () -> wasSideEffectExecuted <- true)
+        Assert.AreSame(original, result)
+        Assert.IsTrue wasSideEffectExecuted
+    
+    [<Test>]
+    let ``Executing a finally side effect on a failure should indeed execute it``() = 
+        let err = InvalidOperationException()
+        let original = Try.Failure<int> err
+        let mutable wasSideEffectExecuted = false
+        let result = original.Finally(fun () -> wasSideEffectExecuted <- true)
+        Assert.AreSame(original, result)
+        Assert.IsTrue wasSideEffectExecuted
+    
+    [<Test>]
+    let ``Executing a failing finally side effect on a success should result in failure``() = 
+        let err = InvalidOperationException()
+        let original = Try.Success 3
+        let result = original.Finally(fun () -> raise err)
+        Assert.AreEqual(Try.Failure<int> err, result)
+    
+    [<Test>]
+    let ``Executing a failing finally side effect on a failure should result in failure``() = 
+        let err = InvalidOperationException()
+        let newErr = NotSupportedException()
+        let original = Try.Failure<int> err
+        let result = original.Finally(fun () -> raise newErr)
+        Assert.AreEqual(Try.Failure<int> newErr, result)
